0506b977a713e50fd58008006ec4ca15d10c310d
shokHorizon <kostan0008@gmail.com>
2022-12-14 21:07:09 +0300

    Zalupa elzina

---
+++ src/MainClient.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.net.Inet4Address;
+
+public class MainClient {
+    public static void main(String[] args) {
+        Inet4Address ip;
+        int port;
+
+        try{
+
+        } catch (IOException exception){
+
+        }
+    }
+}
---
+++ src/Server/ClientHandler.java
@@ -0,0 +1,24 @@
+package Server;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+    private Socket client;
+    private DataInputStream in;
+    private DataOutputStream out;
+    public ClientHandler(Socket client) throws IOException {
+        this.client = client;
+        in = new DataInputStream(client.getInputStream());
+        out = new DataOutputStream(client.getOutputStream());
+    }
+
+    @Override
+    public void run() {
+        try{
+            while (true){
+                String request = in.readLine();
+            }
+        } catch (IOException ignore) {}
+    }
+}
