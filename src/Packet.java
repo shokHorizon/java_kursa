b4902b512f123fba0375e430a52a3b8207c0f7ea
shokHorizon <kostan0008@gmail.com>
2022-12-15 18:36:56 +0300

    Packet

--- src/Packet.java
+++ src/Packet.java
@@ -1,3 +1,17 @@
-public class Packet {
+import MVC.IModel;
 
+import java.util.stream.Stream;
+
+public class Packet {
+    int code;
+    int object_count;
+    IModel models;
+
+    public Packet(Stream stream){
+
+    }
+
+    public void write_to_stream(Stream stream){
+        return;
+    }
 }
