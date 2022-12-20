9e8df600ae195291cabe2050991f81d94f3ea5ca
GitGudSoftEng <onlyforedupurposes18@gmail.com>
2022-12-20 23:04:36 +0300

    Создание класса DBWorker и подготовка main'a к обработке запросов

---
+++ src/Entity/Tours.java
@@ -0,0 +1,39 @@
+package Entity;
+
+import java.util.Objects;
+
+
+public class Tours {
+    private int id;
+    private String city;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tours tours = (Tours) o;
+        return id == tours.id && Objects.equals(city, tours.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, city);
+    }
+}
