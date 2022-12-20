56ea0a962fbc9742c2fdb34c7cd8a121700952cd
shokHorizon <kostan0008@gmail.com>
2022-12-20 23:17:27 +0300

    ToursDao

--- src/MVC/DAO/ToursDao.java
+++ src/MVC/DAO/ToursDao.java
@@ -1,33 +1,44 @@
 package MVC.DAO;
 
 import MVC.IDao;
+import MVC.Models.ToursModel;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class ToursDao implements IDao {
+public class ToursDao implements IDao<ToursModel> {
+    private List<ToursModel> toursModels = new ArrayList<>();
+
+    public ToursDao(){};
+    public ToursDao(ToursModel toursModel){
+        toursModels.add(toursModel);
+    }
+    public ToursDao(List<ToursModel> toursModels){
+        this.toursModels.addAll(toursModels);
+    }
     @Override
-    public Optional get(long id) {
+    public Optional<ToursModel> get(long id) {
         return Optional.empty();
     }
 
     @Override
-    public List getAll() {
+    public List<ToursModel> getAll() {
         return null;
     }
 
     @Override
-    public void save(Object o) {
-
+    public void save(ToursModel toursModel) {
+        return;
     }
 
     @Override
-    public void update(Object o, String[] params) {
-
+    public void update(ToursModel toursModel, String[] params) {
+        return;
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(ToursModel toursModel) {
+        toursModels.remove(toursModel);
     }
 }
