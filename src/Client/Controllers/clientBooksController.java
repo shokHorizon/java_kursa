9c67cdac2bbeedf8f4728efb6de4fee4964f9c82
GitGudSoftEng <onlyforedupurposes18@gmail.com>
2023-01-10 18:16:47 +0300

    бронирование

--- src/Client/Controllers/clientBooksController.java
+++ src/Client/Controllers/clientBooksController.java
@@ -1,10 +1,22 @@
 package Client.Controllers;
 
+import Client.App;
+import Client.QueryController;
+import Models.Books;
+import Models.Travels;
+import Protocols.Packet;
+import Protocols.QueryMethod;
+import Protocols.QueryModel;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+
+import java.util.List;
 
 public class clientBooksController {
 
@@ -15,21 +27,33 @@ public class clientBooksController {
     private Button btnRemove;
 
     @FXML
-    private ListView<?> tourList;
+    private ListView<Books> tourList;
 
     @FXML
-    void btnBackClick(ActionEvent event) {
-
+    void btnBackClick(Event event) {
+        App.setClientMain(); // ?
     }
 
     @FXML
-    void btnRemoveClick(ActionEvent event) {
+    void btnRemoveClick(Event event) {
 
     }
 
     @FXML
     void tourListClick(MouseEvent event) {
 
+    }
+
+    public static clientBooksController instance;
+
+    @FXML
+    public void initialize(){
+        instance = this;
+
+//        Packet packet = new Packet(QueryModel.Books, QueryMethod.Read,null);
+//        System.out.println("Я МЕНЯЮ TRAVELS");
+//        List<Books> models = (List<Books>) QueryController.query_request(packet);
+//        tourList.setItems(FXCollections.observableArrayList(models));
     }
 
 }
--- src/Client/Controllers/clientMainController.java
+++ src/Client/Controllers/clientMainController.java
@@ -1,14 +1,14 @@
 package Client.Controllers;
 
+import Client.App;
 import Client.QueryController;
-import Models.Cities;
-import Models.Countries;
-import Models.Travels;
-import Models.TravelsRepr;
+import Models.*;
 import Protocols.Packet;
 import Protocols.QueryMethod;
 import Protocols.QueryModel;
+import com.sun.javafx.collections.ImmutableObservableList;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -16,11 +16,11 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class clientMainController {
 
@@ -46,52 +46,78 @@ public class clientMainController {
     private TableColumn<TravelsRepr, Integer> tablePrice;
 
     @FXML
-    private ComboBox<Integer> tourCity;
+    private ComboBox<String> tourCity;
 
     @FXML
-    private ComboBox<Integer> tourCountry;
+    private ComboBox<String> tourCountry;
 
     @FXML
     private ImageView tourImage;
 
     @FXML
-    private ComboBox<Integer> tourType;
+    private ComboBox<String> tourType;
 
 
 
     @FXML
     void btnBookClick(ActionEvent event) {
-
+        if (!table.getSelectionModel().isEmpty())
+        {
+            Books books = new Books(0,table.getSelectionModel().getSelectedItem().getId(),0,0);
+            QueryController.query_request(new Packet<>(QueryModel.Books,QueryMethod.Create,books));
+        }
     }
 
     @FXML
     void btnBooksClick(ActionEvent event) {
         // вызов брони и нужна проверка на выбранный поля в списках
-
+        App.setClientBooks();
 
     }
 
     @FXML
     void countryAction(ActionEvent event) {
-
+        if (Objects.equals(tourCountry.getValue(), "")) {
+            tourCountry.setPromptText("Страна");
+            System.out.println(tourCountry.getPromptText() + " аааааааааааааааааааааааааааааааа");
+        }
+        comboUpdate();
     }
 
     @FXML
-    void tableClick(MouseEvent event) {
-
+    void cityAction(ActionEvent event) {
+        comboUpdate();
     }
 
     @FXML
     void tourTypeAction(ActionEvent event) {
-
+        comboUpdate();
     }
 
+    @FXML
+    void tableClick(MouseEvent event) {
+        if (!table.getSelectionModel().isEmpty())
+        {
+            try {
+                tourImage.setImage(new Image(table.getSelectionModel().getSelectedItem().getImage()));
+            } catch (IllegalArgumentException e)
+            {
+                tourImage.setImage(null);
+                System.out.println("Корявая ссылка");
+            }
+        }
+    }
+    private boolean isUpdating = false;
+
     public void updateTravels(TravelsRepr travel){
+        isUpdating = true;
         Packet packet = new Packet(QueryModel.Travels, QueryMethod.Read,travel);
         System.out.println("Я МЕНЯЮ TRAVELS REPR");
         List<TravelsRepr> models = (List<TravelsRepr>) QueryController.query_request(packet);
         System.out.println("Че пришло " + models);
         table.setItems(FXCollections.observableArrayList(models));
+        updateCombos(models);
+        isUpdating = false;
     }
 
     public static clientMainController instance;
@@ -104,6 +130,40 @@ public class clientMainController {
 
     private HashMap<String,Integer> travelTypeToId = new HashMap<>();
     private HashMap<Integer,String> idToTravelType = new HashMap<>();
+
+
+    public void updateCombos(List<TravelsRepr> travels){
+       HashSet<String> typesLst = new HashSet<>();
+       HashSet<String> countriesLst = new HashSet<>();
+       HashSet<String> citiesLst = new HashSet<>();
+
+        typesLst.add("");
+        countriesLst.add("");
+        citiesLst.add("");
+        for (TravelsRepr repr: travels)
+        {
+            typesLst.add(repr.getType());
+            countriesLst.add(repr.getCountry());
+            citiesLst.add(repr.getCity());
+        }
+
+        tourType.setItems(FXCollections.observableArrayList(typesLst));
+        tourCity.setItems(FXCollections.observableArrayList(citiesLst));
+        tourCountry.setItems(FXCollections.observableArrayList(countriesLst));
+    }
+
+    private void comboUpdate(){
+        if (!isUpdating) {
+            TravelsRepr travelsRepr = new TravelsRepr(0,null,null,null,null,0,null);
+            if (!tourType.getSelectionModel().isEmpty())
+                travelsRepr.setType(tourType.getValue());
+            if (!tourCity.getSelectionModel().isEmpty())
+                travelsRepr.setCity(tourCity.getValue());
+            if (!tourCountry.getSelectionModel().isEmpty())
+                travelsRepr.setCountry(tourCountry.getValue());
+            updateTravels(travelsRepr);
+        }
+    }
 
     @FXML
     public void initialize(){
